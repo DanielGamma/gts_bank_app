14cf4bd279e9938af8e17d16b5e7da0396f35774
millard2santos <millard2santos@gmail.com>
2023-02-28 12:18:41 +0100

    tailwindConfig, font-Family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'gray-dark' : '#414A61',
+      'blue-link' : '#0066F6',
+      'white-faded': '#F9F9F9',
+      'dark-blue': '#001533',
+      'white-form': '#EEEEEECC',
+      'gray-card' : '#CECECE',
+      'gray-nav': '#8D8D8D' ,
+      'gray-records': '#292929',
+      'gray-bgIcons': '#6A6A6A',
+      'gray-icons': '#DDDDDD',
+      'light-blue': '#7485B4',
+      'gray-dots' : '#6C6C6C'
+     },
     extend: {},
   },
   plugins: [],

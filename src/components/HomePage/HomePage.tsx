966d5fd020ea05d9c5477c77d01c881e461b3e5e
millard2santos <millard2santos@gmail.com>
2023-02-28 18:00:58 +0100

    fix tailwind config

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,13 +3,17 @@ type Props = {}
 export const HomePage: React.FC<Props> = (props): JSX.Element => {
   return (
     <>
-      <div className=" w-full h-screen bg-gray-dark -z-10 ">
+      <div className="absolute flex flex-col items-center w-96 h-50 gap-5 bg-gray-dark z-10 ">
         <div className="flex w-full justify-between">
-          <div className="flex flex-col gap-1">
-            <p>Andrey Utley</p>
-            <p>ES5421008783867371671186</p>
+          <div className="flex flex-col">
+            <p className="text-2xl text-white-faded font-medium">Andrey Utley</p>
+            <p className="text-lg text-gray-card font-medium">ES5421008783867371671186</p>
           </div>
           <img src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg" alt="" className="w-16 h-16 rounded-full" />
+        </div>
+          <div className="">
+          <p className="text-white-faded text-[26px] font-semibold">8.640,00 €</p>
+          <p className="text-gray-nav font-medium">Available Balance</p>
         </div>
       </div>
     </>

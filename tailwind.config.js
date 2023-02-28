3ec20abc19fb8b86081121fb1cb450f53945ea74
millard2santos <millard2santos@gmail.com>
2023-02-28 12:35:00 +0100

    FontFamily Karla Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,23 @@ module.exports = {
   ],
   theme: {
     colors: {
-      'gray-dark' : '#414A61',
-      'blue-link' : '#0066F6',
+      'gray-dark': '#414A61',
+      'blue-link': '#0066F6',
       'white-faded': '#F9F9F9',
       'dark-blue': '#001533',
       'white-form': '#EEEEEECC',
-      'gray-card' : '#CECECE',
-      'gray-nav': '#8D8D8D' ,
+      'gray-card': '#CECECE',
+      'gray-nav': '#8D8D8D',
       'gray-records': '#292929',
       'gray-bgIcons': '#6A6A6A',
       'gray-icons': '#DDDDDD',
       'light-blue': '#7485B4',
-      'gray-dots' : '#6C6C6C'
-     },
+      'gray-dots': '#6C6C6C'
+    },
+    fontFamily: {
+      Karla: ['Karla', 'sans-serif']
+    },
+
     extend: {},
   },
   plugins: [],

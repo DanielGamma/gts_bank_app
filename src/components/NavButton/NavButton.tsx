663cc0d5079af5ce5c007b0b2dbc48a4c1adc130
rochamiguel1 <miroga2005@gmail.com>
2023-03-02 15:44:50 +0100

    icons with lowercase and classname fixed

--- src/components/NavButton/NavButton.tsx
+++ src/components/NavButton/NavButton.tsx
@@ -4,10 +4,9 @@ import { Link } from "react-router-dom"
 type Props = { icon: string, text: string, link: string }
 
 export const NavButton: React.FC<Props> = ({ icon, text, link }): JSX.Element => {
-    let className = "flex flex-col text-gray-nav hover:scale-110 justify-center items-center gap-2"
 
     return (
-        <div className={className} >
+        <div className="flex flex-col text-gray-nav hover:scale-110 justify-center items-center gap-2" >
             <Link to={link}>
                 <img className="focus:w-9" src={icon} alt="" />
             </Link>

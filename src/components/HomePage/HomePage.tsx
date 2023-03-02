738f8561d736c21afdd11bc1e600f9a81ec4ae3f
ceci <ceciliasegura.t@hotmail.com>
2023-03-02 10:08:06 +0100

    finished transfer_buttons

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,3 +1,6 @@
+import icono from '../../assets/transferbutton.png'
+import iconmob from '../../assets/mobile.png'
+import { TransferButton } from '../TransferButton/TransferButton'
 type Props = {}
 
 export const HomePage: React.FC<Props> = (props): JSX.Element => {
@@ -11,9 +14,21 @@ export const HomePage: React.FC<Props> = (props): JSX.Element => {
           </div>
           <img src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg" alt="" className="w-16 h-16 rounded-full" />
         </div>
-          <div className="">
+        <div className="">
           <p className="text-white-faded text-[26px] font-semibold">8.640,00 €</p>
           <p className="text-gray-nav font-medium">Available Balance</p>
+        </div>
+
+        <div className='flex gap-[55px]'>
+          <div >
+            <TransferButton url={"transactions/transfer text-base"} icon={icono}/>
+            <p className='font-karla font-normal alings-center text-white-form pt-2 '>Transfer</p>
+          </div>
+
+          <div>
+            <TransferButton url={"transactions/bizum"} icon={iconmob}/>
+            <p  className='font-karla font-normal	alings-center text-white-form pl-2 pt-2' >Bizum</p>
+          </div>
         </div>
       </div>
     </>

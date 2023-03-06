f8828321639cae999a0a3cf92ce471f1996f0085
ceci <ceciliasegura.t@hotmail.com>
2023-03-06 20:39:54 +0100

    add user from context

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,7 @@
 
 
-import React from "react"
+import React, { useContext } from "react"
+import { corregir, UserContext } from "../../context/UserProvider"
 import { NavMenu } from "../NavMenu/NavMenu"
 import { OptionMenu } from "../OptionMenu/OptionMenu"
 import { ProfileCard } from "../ProfileCard/ProfileCard"
@@ -10,11 +11,12 @@ type Props = { }
 export const ProfilePage: React.FC<Props> = (): JSX.Element => {
 
 
+    const { currentUser } = useContext(UserContext) as corregir; 
 
     return (
         <div>
             <p className="text-2xlfont-medium text-white-faded text-center p-2 mb-2">Profile</p>
-            <ProfileCard/>
+            <ProfileCard img={currentUser.profile_picture} title={currentUser.first_name} subtitle={currentUser.last_name}/>
             <OptionMenu/>
             <NavMenu />
         </div>

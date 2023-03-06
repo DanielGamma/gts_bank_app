b3cf892b2b74541f94a58432fb3dec7d6c6dcbd8
Martin Balverde <tinchoabalverde@gmail.com>
2023-03-06 12:02:22 +0100

    Avances en expenses page

--- src/components/ExpensesGraphic/ExpensesGraphic.tsx
+++ src/components/ExpensesGraphic/ExpensesGraphic.tsx
@@ -1,4 +1,8 @@
 import { PieChart, Pie, Cell  } from "recharts";
+import { getTransactions, getUser } from "../../services/firebaseFunctions";
+import { useEffect, useState } from "react";
+import { Transaction } from "../../services/interfaces";
+
 
 type Props = {};
 
@@ -19,24 +23,73 @@ export const ExpensesGraphic: React.FC<Props> = (props): JSX.Element => {
     { name: "Leisure", value: 350 }
   ];
 
-  const COLORS = ["#FF764A", "#FFA600", "#003F5C", "#374C80","#7A5195", "#BC5090", "#EF5675"];
+  const colors = ["#FF764A", "#FFA600", "#003F5C", "#374C80","#7A5195", "#BC5090", "#EF5675","#2ABF31","#29B6E5"];
+  
+  const [expenses, setExpenses] = useState<Transaction[] | []>([])
+  
+  useEffect(() => {
+    getUser("IOlHrqIY6Ze7CwbLaj0w5TepRvA3")
+    .then(res=> getTransactions(res.transactions).then(res => setExpenses(res.filter(transac => transac.type === 0))))
+  }, [])
+  
+
+  const expensesForHealth = expenses.filter(expense => expense.category === "Health")
+  const expensesForLeisure = expenses.filter(expense => expense.category === "Leisure")
+  const expensesForEducation = expenses.filter(expense => expense.category === "Education")
+  const expensesForCommuting = expenses.filter(expense => expense.category === "Commuting")
+  const expensesForHome = expenses.filter(expense => expense.category === "Home")
+  const expensesForTraveling = expenses.filter(expense => expense.category === "Traveling")
+  const expensesForGrocery = expenses.filter(expense => expense.category === "Grocery")
+  const expensesForClothes = expenses.filter(expense => expense.category === "Clothes")
+  const expensesForOthers = expenses.filter(expense => expense.category === "Others")
+
+
+  function expensesByCategory(someCategory:any[]) {
+    return someCategory.reduce((acc, e) => {
+      acc.amount += Math.trunc(e.amount);
+      acc.name = e.category
+      return acc
+    }, {
+      name: '',
+      amount: 0
+    });
+  } 
+
+  const allExpensesByCategory:Transaction[] = [
+    expensesByCategory(expensesForHealth),
+    expensesByCategory(expensesForLeisure),
+    expensesByCategory(expensesForEducation),
+    expensesByCategory(expensesForCommuting),
+    expensesByCategory(expensesForHome),
+    expensesByCategory(expensesForTraveling),
+    expensesByCategory(expensesForGrocery),
+    expensesByCategory(expensesForClothes),
+    expensesByCategory(expensesForOthers),
+  ]
+  
+  const categories:String[] = [ ...new Set(expenses.map(expense => expense.category))]
+  
+
+  console.log(allExpensesByCategory);
+
+
 
   return (
 
     <div className="text-white">
             <PieChart width={380} height={310} >
             <Pie
-            data={data}
+            data={allExpensesByCategory}
             cx={180}
             cy={150}
             innerRadius={65}
             outerRadius={120}
             fill="#8884d8"
             paddingAngle={0}
-            dataKey="value"
+            dataKey="amount"
             >
             {data.map((entry, index) => (
-                <Cell key={index} fill={COLORS[index % COLORS.length]} />
+                <Cell key={index} fill={colors[index % colors.length]} />
             ))}
             </Pie>
         </PieChart>
@@ -46,8 +99,8 @@ export const ExpensesGraphic: React.FC<Props> = (props): JSX.Element => {
             {
                 data.map((e,i) => {
                     return <div key={i} className="flex items-center mb-1 mr-3">
-                            <div className={`w-2 h-2 mr-1 rounded-full bg-[${COLORS[i]}]`}></div>
-                            <p className="text-[10px]">{data[i].name}</p>
+                            <div className={`w-2 h-2 mr-1 rounded-full bg-[${colors[i]}]`}></div>
+                            <p className="text-[10px]">{categories[i]}</p>
                         </div> 
                 })
             }
--- src/components/ExpensesPage/ExpensesPage.tsx
+++ src/components/ExpensesPage/ExpensesPage.tsx
@@ -2,12 +2,21 @@
 import { ExpensesGraphic } from "../ExpensesGraphic/ExpensesGraphic";
 import { NavMenu } from "../NavMenu/NavMenu"
 import { Header } from "../Header/Header"
+import { getTransactions, getUser } from "../../services/firebaseFunctions";
+import { useEffect, useState } from "react";
+import { Transaction } from "../../services/interfaces";
 
 type Props = {};
 
 export const ExpensesPage: React.FC<Props> = (props): JSX.Element => {
+  
+  const [expenses, setExpenses] = useState<Transaction[] | []>([])
 
-  const testArray:number[] = [1,2,3,4]
+  useEffect(() => {
+    getUser("IOlHrqIY6Ze7CwbLaj0w5TepRvA3")
+    .then(res=> getTransactions(res.transactions).then(res => setExpenses(res.filter(transac => transac.type === 0))))
+  }, [])
+  
 
   return (
     <>
@@ -22,19 +31,19 @@ export const ExpensesPage: React.FC<Props> = (props): JSX.Element => {
 
       <h3 className="text-white mt-6 text-[20px] text-[#EEEEEECC]">Expenses Record</h3>
 
-      <div className="flex flex-col gap-5 bg-gray-records px-5 py-4 rounded-[20px]">
-        {testArray.map((test, i) => {
+      <div className="flex flex-col mb-20 gap-5 bg-gray-records px-5 py-4 rounded-[20px]">
+        {expenses.map((expense, i) => {
           return (
             <div key={i}>
               <div className="w-full flex justify-between items-center text-white-faded ">
                 <article className="flex gap-4 items-center">
                   <div className="w-[76px] h-[76px] bg-light-blue rounded-full"></div>
-                  <p>Clothes</p>
+                  <p>{expense.receiver_name}</p>
                 </article>
-                <p>- €400</p>
+                <p>- €{expense.amount}</p>
               </div>
               {
-                i < testArray.length - 1 ? (<div className="h-px w-full mt-4 bg-gray-400"></div>) : ("")
+                i < expenses.length - 1 ? (<div className="h-px w-full mt-4 bg-gray-400"></div>) : ("")
               }
             </div>
           );

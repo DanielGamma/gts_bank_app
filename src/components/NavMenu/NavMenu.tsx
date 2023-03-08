bc479e34536ff49f3ff4d5f05ba877fb5bf08009
DanielGamma <daniel.carrasco@gammatech.school>
2023-03-08 10:12:26 +0100

    Change capital name files in imports

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,12 +1,12 @@
 import { NavButton } from "../NavButton/NavButton"
-import Home from "../../assets/Home.svg"
+import Home from "../../assets/home.svg"
 import transactions from "../../assets/transactions.svg"
 import profile from "../../assets/profile.svg"
-import navcards from "../../assets/Card.svg"
+import navcards from "../../assets/card.svg"
 import HomeWhite from "../../assets/Home-white.svg"
 import transactionsWhite from "../../assets/transactions-white.svg"
 import profileWhite from "../../assets/profile-white.svg"
-import navcardsWhite from "../../assets/CardWhite.svg"
+import navcardsWhite from "../../assets/cardWhite.svg"
 
 
 type Props = {}

1ad4e623f324362638dde7fb19b8f7ce4d12e4f9
DanielGamma <daniel.carrasco@gammatech.school>
2023-03-09 13:27:12 +0100

    Rework cardsPage structure: MyCards is a usless component and poorly named, hence it's deleted

--- src/components/CardsPage/CardsPage.tsx
+++ src/components/CardsPage/CardsPage.tsx
@@ -1,7 +1,11 @@
+import detail from '../../assets/detail.png'
+import newCard from '../../assets/newCard.png'
+import cvvPin from '../../assets/cvvPin.png'
+import remove from '../../assets/remove.png'
 import { Header } from "../Header/Header"
 import { NavMenu } from "../NavMenu/NavMenu"
 import Carousel from "../Carousel/Carousel"
-import MyCards from "../MyCards/MyCards"
+import { OptionMenu } from "../OptionMenu/OptionMenu"
 
 
 type Props = {}
@@ -11,7 +15,9 @@ export const CardsPage: React.FC<Props> = (props):JSX.Element => {
     <>
     <Header arrow={false} content={"My Cards"} url={""}/>
     <Carousel />
-    <MyCards />
+    <div className="w-full bg-gray-records rounded-3xl mt-6">
+      <OptionMenu icons={[detail, newCard, cvvPin, remove]} texts={["Card Details", "New Card", "Check CVV & PIN", "Remove Card"]} urls={["/cards/details", "/cards/new", "/cards/secret", "/cards"]} />
+    </div>
     <NavMenu/>
     </>
   )
